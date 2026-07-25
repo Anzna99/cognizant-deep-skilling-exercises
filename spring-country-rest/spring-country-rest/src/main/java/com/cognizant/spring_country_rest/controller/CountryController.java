66c01ecb5d26868a4550a7040fe058b76140879e
Anzna99 <anzna999@gmail.com>
2026-07-25 08:54:13 +0530

    spring

---
+++ spring-country-rest/spring-country-rest/src/main/java/com/cognizant/spring_country_rest/controller/CountryController.java
@@ -0,0 +1,22 @@
+package com.cognizant.spring_country_rest.controller;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.spring_country_rest.model.Country;
+
+@RestController
+public class CountryController {
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+
+        Country country = context.getBean("india", Country.class);
+
+        return country;
+    }
+}
